fb3433ad81f1b3a61a50fa0c2d54e60fdfc2d55c
seyedrezabazyar <seyedrezabazyar@gmail.com>
2022-08-29 12:12:21 +0430

    what is artisan?

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('test', function (){
+    var_dump('Hello world from console');
+})->purpose('just for test');
