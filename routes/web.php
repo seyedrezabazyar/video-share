3580928bd49b877f1ef9b7fabc3e8d42f12f11fa
seyedrezabazyar <seyedrezabazyar@gmail.com>
2022-08-30 14:24:02 +0430

    laravel formatting with inheritance

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [\App\Http\Controllers\IndexController::class, 'index']);
+
+Route::get('/upload', function (){
+    return view('videos.create');
+});
