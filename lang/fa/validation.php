cb6ccf7e03181be4f416ebffee416c8bd2febd2e
seyedrezabazyar <seyedrezabazyar@gmail.com>
2022-08-31 15:07:43 +0430

    translation of validation errors

---
+++ lang/fa/validation.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'required' => ':attribute الزامی می‌باشد.',
+    'attributes' => [
+        'name' => 'نام ویدیو',
+        'length' => 'مدت زمان',
+        'slug' => 'نام یکتا',
+        'url' => 'آدرس ویدیو',
+        'thumbnail' => 'تصویر بند انگشتی',
+    ],
+];
